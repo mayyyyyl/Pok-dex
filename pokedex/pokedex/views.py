ab5dbc539e814ec596d6ee0857a5cb31917e09bf
Maylis Le Borgne <maylislb@hotmail.fr>
2022-11-08 08:32:08 +0100

    ajout d'un fichier styel.css, ajout html sur le template pokemon

--- pokedex/pokedex/views.py
+++ pokedex/pokedex/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
 from django.shortcuts import render
+import requests
 
 # liste de pokémon dans la limite de 151
 urlPokedex = 'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0' 
@@ -9,7 +10,7 @@ def index(request):
   return render(request, "index.html")
 
 def pokemon(request):
-  req = request.get(urlPokedex)
+  req = requests.get(urlPokedex)
   wb = req.json()
-  context = {"pokemon_list": wb}
+  context = {"pokemon_list": wb['results']}
   return render(request, "pokemon.html", context)
